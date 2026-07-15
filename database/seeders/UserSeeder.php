195fa8a2d6716fd747fbe1bafc8f499796063118
Babsi-08 <babsooo@icloud.com>
2026-07-15 11:05:32 +0200

    Add UserSeeder with test accounts and remove example test user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Seed the application's users.
+     */
+    public function run(): void
+    {
+        $users = [
+            ['name' => 'Ba', 'email' => 'ba.test@aon.at', 'password' => 'testba'],
+            ['name' => 'Be', 'email' => 'be.test@aon.at', 'password' => 'testbe'],
+            ['name' => 'J', 'email' => 'j.test@aon.at', 'password' => 'testj'],
+        ];
+
+        foreach ($users as $user) {
+            User::updateOrCreate(
+                ['email' => $user['email']],
+                [
+                    'name' => $user['name'],
+                    'password' => Hash::make($user['password']),
+                    'email_verified_at' => now(),
+                ],
+            );
+        }
+    }
+}
